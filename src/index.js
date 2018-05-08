7f45e860226de7fcdb1810ca695131017f28c4fb
Paweł Samsel <pawel.samsel@absolvent.pl>
2018-05-08 11:48:46 +0200

    Rename component

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ export {
   PhoneScreen, PhoneTabletScreen, TabletScreen, TabletDesktopScreen, DesktopScreen,
 } from './components'
 export { breakPoints } from './defaults';
-export { hoc as ReactResponsiveNextHoc } from './hoc';
+export { hoc as ReactResponsiveConnect } from './hoc';
