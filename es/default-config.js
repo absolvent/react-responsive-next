c99b50ccd550c17ddc75457403c95d6616c3b9cd
Paweł Samsel <pawel.samsel@absolvent.pl>
2018-05-29 17:12:35 +0200

    Update current build

---
+++ es/default-config.js
@@ -0,0 +1,25 @@
+export var TAG = '[react-responsive-next]';
+export var defaultConfig = {
+  breakPoints: {
+    sm: {
+      to: 767
+    },
+    md: {
+      from: 768,
+      to: 1239
+    },
+    lg: {
+      from: 1240
+    }
+  },
+  devicesToBreakPoints: {
+    phone: 'sm',
+    tablet: 'md',
+    car: 'md',
+    desktop: 'lg',
+    tv: 'lg',
+    bot: 'lg',
+    undefined: 'lg'
+  }
+};
+//# sourceMappingURL=default-config.js.map
